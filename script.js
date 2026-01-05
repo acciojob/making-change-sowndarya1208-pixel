70054a192b7901beae76f9258b69a57f3daa5674
sowndarya1208-pixel <sowndarya1208@gmail.com>
2026-01-05 20:10:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const makeChange = (c) => {
   // your name here
-};
+	let q = Math.floor(amount / 25);
+  amount = amount % 25;
+
+  let d = Math.floor(amount / 10);
+  amount = amount % 10;
+
+  let n = Math.floor(amount / 5);
+  amount = amount % 5;
+
+  let p = amount; // remaining pennies
+
+  return {
+    q: q,
+    d: d,
+    n: n,
+    p: p
+  };
+}
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
